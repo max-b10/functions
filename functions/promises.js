def9980dce5aba036d48cea2e59fb7a2b3d70b9b
max-b10 <bungaymax@gmail.com>
2021-02-17 10:12:24 +0000

    Promise chaining

--- functions/promises.js
+++ functions/promises.js
@@ -1,34 +1,62 @@
 // Callback
-const getDataCallback = (callback) => {
+const getDataCallback = (num, callback) => {
   setTimeout(() => {
+    if (typeof num === "number") {
+      callback(undefined, num * 2);
+    } else {
+      callback("Number must be provided");
+    }
     // callback(undefined, "This is the data");
-    callback("This is my callback error", undefined);
+    // callback("This is my callback error", undefined);
   }, 2000);
 };
-getDataCallback((err, data) => {
+getDataCallback(2, (err, data) => {
   if (err) {
     console.log(err);
   } else {
-    console.log(data);
+    getDataCallback(data, (err, data) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(data);
+      }
+    });
   }
 });
 
 //Promise
-const getDataPromise = (data) =>
+const getDataPromise = (num) =>
   new Promise((resolve, reject) => {
     setTimeout(() => {
-      resolve(`This is the promise data: ${data}`);
+      typeof num === "number"
+        ? resolve(num * 2)
+        : reject("Number must be provided");
+      // resolve(`This is the promise data: ${data}`);
       //   reject("This is my promise error");
     }, 3000);
   });
 
-const myPromise = getDataPromise(123);
-
-myPromise.then(
-  (data) => {
+getDataPromise(10)
+  .then((data) => {
+    return getDataPromise(data);
+  })
+  .then((data) => {
+    return getDataPromise(data);
+  })
+  .then((data) => {
     console.log(data);
-  },
-  (err) => {
+  })
+  .catch((err) => {
     console.log(err);
-  }
-);
+  });
+
+// const myPromise = getDataPromise(123);
+
+// myPromise.then(
+//   (data) => {
+//     console.log(data);
+//   },
+//   (err) => {
+//     console.log(err);
+//   }
+// );
